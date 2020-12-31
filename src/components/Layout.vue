b148ca956aa0ad479d1469d350d0da00f5e05e8d
lim-mil <limyel@outlook.com>
2020-12-31 23:53:36 +0800

    布局

---
+++ src/components/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container>
+    <el-aside>
+
+    </el-aside>
+    <el-container>
+      <el-header></el-header>
+      <el-header></el-header>
+
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: "Layout"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/utils/element.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import {
+  Container,
+  Aside,
+  Header
+} from 'element-ui'
+
+Vue.use(Container)
