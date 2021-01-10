52181078920dc87f33b1907ea8492fd95b43d771
lim-mil <limyel@outlook.com>
2021-01-10 23:56:12 +0800

    封装axios“

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,10 +2,10 @@
   <div class="login">
     <el-form>
       <el-form-item label="用户名">
-        <el-input type="text"></el-input>
+        <el-input v-model="username" type="text"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input type="password"></el-input>
+        <el-input v-model="password" type="password"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">登录</el-button>
@@ -17,7 +17,13 @@
 
 <script>
 export default {
-name: "Login"
+  name: "Login",
+  data: () => {
+    return {
+      username: '',
+      password: '',
+    }
+  }
 }
 </script>
 
